63cd19a5bb6245ff7be729a7ecec465d8702134c
HARRY PRIOR <harryprior@hotmail.co.uk>
2022-02-18 08:29:09 +0000

    update

--- FSharpPDev/HigherPlaneOfExistence.fs
+++ FSharpPDev/HigherPlaneOfExistence.fs
@@ -15,12 +15,11 @@ A composed function, once lifted, should be the same as a composed function, 'un
 Sig: E<(a -> b)> -> E<a> -> E<b>
 *)
 
-// Example impl 
+// Example impl
 let mapOption f opt =
     match opt with
     | None -> None
-    | Some x ->
-        Some (f x) // apply function to inner value
+    | Some x -> Some(f x) // apply function to inner value
 
 // Using Map
 
@@ -31,12 +30,12 @@ let add1IfSomething = Option.map add1
 let result = add1IfSomething (Some 1)
 
 // Or...
-let terseOptionMap = Some 1 |>  Option.map add1
+let terseOptionMap = Some 1 |> Option.map add1
 
 // --> Return <--//
 
 (*
-Doesn't need much of an explanation: lifts a value 
+Doesn't need much of an explanation: lifts a value
 Sig: a -> E<a>
 *)
 
@@ -59,12 +58,12 @@ Sig: E<(a -> b -> c)> -> E<a> -> E<b> -> E<c>
 
 let apply fOpt xOpt =
     match fOpt, xOpt with
-    | Some f, Some x -> Some (f x) // Apply first argument to second 
+    | Some f, Some x -> Some(f x) // Apply first argument to second
     | _ -> None
-    
+
 // Using Apply
 
-// Long curried version 
+// Long curried version
 let add x y = x + y
 let someAdd = Some add
 let someAddPartialOne = apply someAdd (Some 1)
@@ -75,16 +74,89 @@ let (<*>) = apply
 let infixResult = (Some add) <*> Some 1 <*> Some 1 // Some 2
 
 
+// --> LiftN <-- //
 
+(*
+What?
+Combines two (or three, or four or n) elevated values using a specified function
+Sig (lift2): (a -> b -> c) -> E<a> -> E<b> -> E<c>
+
+Apply and return can be used to define a series of helper functions
+that turn a normal function into one that can be applied to elevated values
+lift1 is just a map, so it is not used
+*)
+
+// Example impl
+
+// let (<*>) = apply <- already defined
+
+
+let (<!>) = Option.map
+
+let lift2 f x y = f <!> x <*> y // map f to x then apply result to y
+
+// Example usage
+
+let addPair x y = x + y
+
+let optionAddPair = lift2 addPair
+
+let liftResult = optionAddPair (Some 1) (Some 1) // ->  Some 2
+
+// --> Bind <-- //
+
+(*
+What?
+Allows you to compose world crossing (monadic) functions.
+Cross curring functions are of the type: a -> E<a>, for example a function
+which parses a string: string -> Option<int>. We cannot link these functions together with
+standard composition. Bind allows us transform a monadic function into a lifted function - and
+chain them together.
+Sig: (a -> E<b>) -> E<a> -> E<b>
+*)
+
+// Example impl
+
+let bind f xOpt =
+    match xOpt with
+    | Some x -> f x
+    | _ -> None
+
+
+// Example usage
+
+let parseInt str =
+    match str with
+    | "-1" -> Some -1
+    | "0" -> Some 0
+    | "1" -> Some 1
+    | "2" -> Some 2
+    | _ -> None
+
+let checkDivide i =
+    match i with
+    | 0 -> None
+    | i -> Some i
     
-    
+type OrderQty = OrderQty of int
+
+let toOrderQty qty =
+    if qty >= 1 then
+        Some (OrderQty qty)
+    else
+        // only positive numbers allowed
+        None
 
 
+let useBind str = str |> parseInt  |> Option.bind checkDivide
+
+let boundCheckDivide = Option.bind checkDivide
+
+let useBindWithIntermediateFunc str = parseInt str |> boundCheckDivide
 
 
+// Infix version
 
+let (>>=) opt f = Option.bind f opt
 
-
-
-
-
+let combineWithBind str = str |> parseInt >>= checkDivide 
